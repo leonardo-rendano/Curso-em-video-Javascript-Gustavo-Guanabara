60706d20ed704dbeb52d5d7bcccbdeff3fd1ddde
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-21 14:10:35 -0300

    Exercício 13 e aplicação

--- aula012/ex012.js
+++ aula012/ex012.js
@@ -1,11 +1,13 @@
 var agora = new Date()
 var hora = agora.getHours()
 
-if (hora > 6 && hora < 12) {
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora >= 6 && hora <= 12) {
   console.log('Bom dia!')
-} else if (hora > 12 && hora < 18) {
+} else if (hora >= 12 && hora <= 18) {
   console.log('Boa tarde!')
-} else if (hora > 18 && hora < 24) {
+} else if (hora >= 18 && hora <= 24) {
   console.log('Boa noite!')
 } else {
   console.log('Boa madrugada!')
