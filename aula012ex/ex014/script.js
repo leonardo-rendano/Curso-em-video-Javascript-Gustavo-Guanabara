9490e07fba07fe821dd8a14d698b676a7e40fec0
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-21 20:26:39 -0300

    Alteração das imagens

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar() {
+  var msg = document.getElementById('msg')
+  var img = document.getElementById('imagem')
+
+  var data = new Date()
+  var hora = data.getHours()
+  var minutos = data.getMinutes()
+
+  msg.innerHTML = `Agora são ${hora}:${minutos} horas.`
+
+  if (hora >= 0 && hora <= 12) {
+    img.innerHTML = '<img src=manhã.png>'
+    document.body.style.background = '#e2cd9f'
+  } else if (hora > 12 && hora <= 18) {
+    img.innerHTML = '<img src=tarde.png>'
+    document.body.style.background = '#b9846f'
+  } else {
+    img.innerHTML = '<img src=noite.png>'
+    document.body.style.background = '#515154'
+  }
+  
+}
